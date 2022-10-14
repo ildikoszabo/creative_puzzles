2634b1b8e20f072ffbd20ed3538ea1f31f585e1c
ildikoszabo4 <ildikoszabo4@gmail.com>
2022-10-14 19:28:16 +0300

    add all the section to the landing page

--- creative-puzzles/src/pages/LandingPage.js
+++ creative-puzzles/src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import "./LandingPage.css";
 import puzzle_pieces from "../assests/puzzle_pieces.png";
 import patreon from "../assests/Digital-Patreon-Logo_Black.png";
 import game_picture from "../assests/game_picture.PNG";
+import gallery from "../assests/Gallery.png";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -12,8 +13,8 @@ export default function LandingPage() {
   return (
     <div>
       <section class="main">
-        <nav>
-          <ul id="nav-list">
+        <nav class="c-fx-row-space-between">
+          <ul class="c-fx-row-start" id="nav-list">
             <li>
               <headingtitle>puzzle games</headingtitle>
             </li>
@@ -31,7 +32,7 @@ export default function LandingPage() {
             <img class="rotate90 logo" src={puzzle_pieces} alt="puzzle games" />
           </div>
 
-          <div class="nav-right">
+          <div class="c-fx-row-start">
             <Button href="#login" disableElevation>
               Log in
             </Button>
@@ -40,8 +41,8 @@ export default function LandingPage() {
             </Button>
           </div>
         </nav>
-        <div class="main-area">
-          <div class="main-text">
+        <div class="c-fx-row-space-between main-area">
+          <div class="c-fx-column-start c-fx-space-between main-text">
             <h1>Puzzling simplified</h1>
             <p class="c-pb-full ">
               An online platform designed to play and to engage with the
@@ -59,20 +60,20 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <section class="socials">
-          <div class="socials-item">
+        <section class="c-fx-row-space-between socials">
+          <div class="c-fx-column-center c-fx-space-between">
             <IconButton component="label">
               <YouTubeIcon fontSize="large" />
             </IconButton>
             <span>Watch more puzzle related content</span>
           </div>
-          <div class="socials-item">
+          <div class="c-fx-column-center c-fx-space-between">
             <IconButton component="label">
               <img src={patreon} alt="patreon" class="socials-icon-size" />
             </IconButton>
             <span>Join the Patreon community</span>
           </div>
-          <div class="socials-item">
+          <div class="c-fx-column-center c-fx-space-between">
             <IconButton component="label">
               <InstagramIcon fontSize="large" />
             </IconButton>
@@ -80,7 +81,7 @@ export default function LandingPage() {
               For daily and behind the scene content follow on Instagram
             </span>
           </div>
-          <div class="socials-item">
+          <div class="c-fx-column-center c-fx-space-between">
             <IconButton component="label">
               <GitHubIcon fontSize="large" />
             </IconButton>
@@ -90,11 +91,11 @@ export default function LandingPage() {
           </div>
         </section>
       </section>
-      <section class="games-section" id="games">
+      <section class="c-fx-column games-section" id="games">
         <div>
           <h2>Try one of these games now</h2>
         </div>
-        <div class="project-container">
+        <div class="c-fx-space-around c-fx-row project-container">
           <div class="project-card">
             <img
               class="project-image"
@@ -123,8 +124,8 @@ export default function LandingPage() {
           </div>
         </div>
       </section>
-      <section id="community" class="community-section">
-        <div class="community-main">
+      <section id="community" class="c-fx-column community-section">
+        <div class="c-fx-row-space-between">
           <div>
             <img class="rotateminus90" src={puzzle_pieces} alt="puzzle games" />
           </div>
@@ -141,8 +142,89 @@ export default function LandingPage() {
           </Button>
         </div>
       </section>
-      <footer id="contact-me">
-        <h2>Contact me</h2>
+      <section className="games-again-section">
+        <div className="c-fx-column c-fx-space-center c-padding-full">
+          <p>Something with a puzzle image and related to the games</p>
+          <div>
+            <Button variant="contained" href="#games">
+              Play now
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      <section id="about-me" className="c-fx-row-space-between">
+        <div className="c-fx-row">
+          <img src={gallery} alt="personal projects" />
+        </div>
+
+        <div className="c-fx-column">
+          <div className="c-fx-row about-me-icon">
+            <img src={puzzle_pieces} alt="puzzle games" />
+          </div>
+          <div class="c-fx-column-start c-fx-space-between about-me-text ">
+            <h2>About</h2>
+            <p>
+              All the design and implementation done by one passionate
+              individual.
+            </p>
+            <p class="c-pb-full">Meet me.</p>
+            <div>
+              <Button variant="outlined" href="#games">
+                Visit personal webpage
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="contact-me" className="c-fx-row contact-me">
+        <div className="c-fx-column-start c-fx-space-between contact-me-text">
+          <h2>Do you have some feedback? Let’s talk!</h2>
+          <div class="c-pb-full">
+            <Button variant="contained" href="#games">
+              Join a livestream
+            </Button>
+          </div>
+        </div>
+        <div className="c-fx-column-center contact-me-email">
+          <h2>hello@puzzlegame.com</h2>
+        </div>
+      </section>
+
+      <footer className="c-fx-row-space-between">
+        <div className="c-fx-column">
+          <p>Enjoy free to play puzzle games online.</p>
+          <h2>Puzzle games</h2>
+        </div>
+        <div className="c-fx-column footer-item">
+          <span>
+            Puzzle games is an online website which offers free to play games
+          </span>
+          <span style={{ fontWeight: "bold" }}>© Puzzle games 2022</span>
+        </div>
+        <div className="c-fx-column footer-item-bold">
+          <span>
+            <a href="#games">Games</a>
+          </span>
+          <span>
+            <a href="#community">Community</a>
+          </span>
+          <span>
+            <a href="#about-me">About me</a>
+          </span>
+        </div>
+        <div className="c-fx-column footer-item-bold">
+          <span>1 000 123 4566</span>
+          <span>hello@puzzlegame.com</span>
+        </div>
+        <div className="c-fx-column footer-item">
+          <span>
+            <a href="#community">Terms of Use</a>
+          </span>
+          <span>
+            <a href="#community">Privacy policy</a>
+          </span>
+        </div>
       </footer>
     </div>
   );
