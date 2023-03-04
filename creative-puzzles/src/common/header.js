d3224b5e9679d3ec0d46079b6b677bde86f055b5
ildikoszabo4 <ildikoszabo4@gmail.com>
2023-03-04 17:35:26 +0200

    optimize header for small screen

--- creative-puzzles/src/common/header.js
+++ creative-puzzles/src/common/header.js
@@ -1,22 +1,33 @@
-import React from "react";
-import "../index.css";
-import "./header.css";
-import { navStyle } from "./appTheme";
+import React from 'react';
+import '../index.css';
+import './header.css';
+import { navStyle } from './appTheme';
+import DrawerMenu from './components/DrawerMenu';
 
 export default function Header(props) {
   return (
-    <nav className="c-fx-row-space-between" style={navStyle}>
-      <ul className="c-fx-row-start" id="nav-list">
-        <li>
-          <headingtitle>{props.headerTitle}</headingtitle>
-        </li>
-        {props.headerNavLinks.map((el, index) => (
-          <li key={index}>
-            <a href={el.path}>{el.name}</a>
+    <div>
+      <nav className="c-fx-row-space-between listMenu" style={navStyle}>
+        <ul className="c-fx-row-start" id="nav-list">
+          <li>
+            <headingtitle>{props.headerTitle}</headingtitle>
           </li>
-        ))}
-      </ul>
-      {props.children}
-    </nav>
+          {props.headerNavLinks.map((el) => (
+            <li>
+              <a href={el.path}>{el.name}</a>
+            </li>
+          ))}
+        </ul>
+        {props.children}
+      </nav>
+      <nav className="c-fx-row-space-between drawerMenu" style={navStyle} id="drawer-menu">
+        <div className="c-fx-align-self-center" style={{ marginLeft: '80px' }}>
+          <headingtitle>{props.headerTitle}</headingtitle>
+        </div>
+        <div className="c-fx-align-self-center" style={{ height: '100%' }}>
+          <DrawerMenu navLinks={props.headerNavLinks} />
+        </div>
+      </nav>
+    </div>
   );
 }
--- creative-puzzles/src/pages/LandingPage.js
+++ creative-puzzles/src/pages/LandingPage.js
@@ -1,157 +1,128 @@
-import React, { useEffect, useState } from "react";
-import { Button, IconButton } from "@mui/material";
-import "./LandingPage.css";
-import "../index.css";
-import puzzle_pieces from "../assests/puzzle_pieces.png";
-import patreon from "../assests/Digital-Patreon-Logo_Black.png";
-import game_picture from "../assests/game_picture.PNG";
-import gallery from "../assests/Gallery.png";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import { Link } from "react-router-dom";
-import Header from "../common/header";
-import {
-  complementaryLightBg,
-  complementaryMainBg,
-  footerStyle,
-} from "../common/appTheme";
+import React, { useEffect, useState } from 'react';
+import { Button, IconButton } from '@mui/material';
+import './LandingPage.css';
+import '../index.css';
+import puzzle_pieces from '../assests/puzzle_pieces.png';
+import patreon from '../assests/Digital-Patreon-Logo_Black.png';
+import game_picture from '../assests/game_picture.PNG';
+import gallery from '../assests/Gallery.png';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from 'react-router-dom';
+import Header from '../common/header';
+import { complementaryLightBg, complementaryMainBg, footerStyle } from '../common/appTheme';
 
 const headerNavLinks = [
-  { name: "Games", path: "#games" },
-  { name: "Community", path: "#community" },
-  { name: "About me", path: "#about-me" },
+  { name: 'Games', path: '#games' },
+  { name: 'Community', path: '#community' },
+  { name: 'About me', path: '#about-me' },
 ];
 
 export default function LandingPage() {
   return (
     <div>
-      <section className="main">
-        <Header headerTitle="puzzle games" headerNavLinks={headerNavLinks}>
-          <div className="puzzle-piece">
-            <img
-              className="rotate90 logo"
-              src={puzzle_pieces}
-              alt="puzzle games"
-            />
-          </div>
+      <div>
+        <section>
+          <Header headerTitle="puzzle games" headerNavLinks={headerNavLinks}>
+            <div class="puzzle-piece">
+              <img class="rotate90 logo" src={puzzle_pieces} alt="puzzle games" />
+            </div>
 
-          <div className="c-fx-row-start">
-            <Button href="#login" disableElevation>
-              Log in
-            </Button>
-            <Button variant="contained" href="#games" disableElevation>
-              Play
-            </Button>
-          </div>
-        </Header>
-        <div className="c-fx-row-space-between main-area">
-          <div className="c-fx-column-start c-fx-space-between main-text">
-            <h1>Puzzling simplified</h1>
-            <p className="c-pb-full ">
-              An online platform designed to play and to engage with the
-              community — all in one place
-            </p>
-
-            <div className="c-pb-full">
-              <Button variant="contained" href="#games">
-                Try a game
+            <div class="c-fx-row-start">
+              <Button href="#login" disableElevation>
+                Log in
+              </Button>
+              <Button variant="contained" href="#games" disableElevation>
+                Play
               </Button>
             </div>
-          </div>
-          <div className="main-right">
-            <img src={puzzle_pieces} alt="puzzle games" />
-          </div>
-        </div>
+          </Header>
+          <div class="c-fx-row-space-between main-area main">
+            <div class="c-fx-column-start c-fx-space-between main-text">
+              <h1>Puzzling simplified</h1>
+              <p class="c-pb-full ">
+                An online platform designed to play and to engage with the community — all in one
+                place
+              </p>
 
-        <section
-          className="c-fx-row-space-between socials"
-          style={complementaryLightBg}
-        >
-          <div className="c-fx-column-center c-fx-space-between">
+              <div class="c-pb-full">
+                <Button variant="contained" href="#games">
+                  Try a game
+                </Button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="socials" style={complementaryLightBg}>
+          <div class="c-fx-column">
             <IconButton component="label">
               <YouTubeIcon fontSize="large" />
             </IconButton>
-            <span>Watch more puzzle related content</span>
+            <span class="socials-text">Watch more puzzle related content</span>
           </div>
-          <div className="c-fx-column-center c-fx-space-between">
+          <div class="c-fx-column">
             <IconButton component="label">
-              <img src={patreon} alt="patreon" className="socials-icon-size" />
+              <img src={patreon} alt="patreon" class="socials-icon-size" />
             </IconButton>
-            <span>Join the Patreon community</span>
+            <span class="socials-text">Join the Patreon community</span>
           </div>
-          <div className="c-fx-column-center c-fx-space-between">
+          <div class="c-fx-column">
             <IconButton component="label">
               <InstagramIcon fontSize="large" />
             </IconButton>
-            <span>
+            <span class="socials-text">
               For daily and behind the scene content follow on Instagram
             </span>
           </div>
-          <div className="c-fx-column-center c-fx-space-between">
+          <div class="c-fx-column">
             <IconButton component="label">
               <GitHubIcon fontSize="large" />
             </IconButton>
-            <span>
+            <span class="socials-text">
               To find out more about how these games work check out GitHub
             </span>
           </div>
         </section>
-      </section>
-      <section
-        className="c-fx-column games-section"
-        style={complementaryMainBg}
-        id="games"
-      >
+      </div>
+      <section class="c-fx-column games-section" style={complementaryMainBg} id="games">
         <div>
           <h2>Try one of these games now</h2>
         </div>
-        <div className="c-fx-space-around c-fx-row project-container">
-          <div className="project-card">
-            <img
-              className="project-image"
-              src={game_picture}
-              alt="Infinity puzzle"
-            />
+        <div class="c-fx-space-around c-fx-row project-container">
+          <div class="project-card">
+            <img class="project-image" src={game_picture} alt="Infinity puzzle" />
             <h3>Infinity puzzle</h3>
-            <p className="subtext">Infinit scrolling puzzle game</p>
+            <p class="subtext">Infinit scrolling puzzle game</p>
             <hr />
             <h3>
-              <Link className="project-link" to={`games/infinityPuzzle`}>
+              <Link class="project-link" to={`games/infinityPuzzle`}>
                 Play now
               </Link>
             </h3>
           </div>
-          <div className="project-card">
-            <img
-              className="project-image"
-              src={game_picture}
-              alt="Puzzle tetris"
-            />
+          <div class="project-card">
+            <img class="project-image" src={game_picture} alt="Puzzle tetris" />
             <h3>Puzzle tetris</h3>
-            <p className="subtext">classNameic tetris with a puzzle twist</p>
+            <p class="subtext">Classic tetris with a puzzle twist</p>
             <hr />
             <h3>
-              <a className="project-link" href="">
+              <a class="project-link" href="">
                 Play now
               </a>
             </h3>
           </div>
         </div>
       </section>
-      <section id="community" className="c-fx-column community-section">
-        <div className="c-fx-row-space-between">
+      <section id="community" class="c-fx-column community-section">
+        <div class="c-fx-row-space-between">
           <div>
-            <img
-              className="rotateminus90"
-              src={puzzle_pieces}
-              alt="puzzle games"
-            />
+            <img class="rotateminus90" src={puzzle_pieces} alt="puzzle games" />
           </div>
           <div className="community-main-text">
             <p>
-              Join the Patreon community to read free articles about how these
-              games were built and unlock access to livestreams.
+              Join the Patreon community to read free articles about how these games were built and
+              unlock access to livestreams.
             </p>
           </div>
         </div>
@@ -181,13 +152,10 @@ export default function LandingPage() {
           <div className="c-fx-row about-me-icon">
             <img src={puzzle_pieces} alt="puzzle games" />
           </div>
-          <div className="c-fx-column-start c-fx-space-between about-me-text ">
+          <div class="c-fx-column-start c-fx-space-between about-me-text ">
             <h2>About</h2>
-            <p>
-              All the design and implementation done by one passionate
-              individual.
-            </p>
-            <p className="c-pb-full">Meet me.</p>
+            <p>All the design and implementation done by one passionate individual.</p>
+            <p class="c-pb-full">Meet me.</p>
             <div>
               <Button variant="outlined" href="#games">
                 Visit personal webpage
@@ -196,14 +164,10 @@ export default function LandingPage() {
           </div>
         </div>
       </section>
-      <section
-        id="contact-me"
-        style={complementaryMainBg}
-        className="c-fx-row contact-me"
-      >
+      <section id="contact-me" style={complementaryMainBg} className="c-fx-row contact-me">
         <div className="c-fx-column-start c-fx-space-between contact-me-text">
           <h2>Do you have some feedback? Let’s talk!</h2>
-          <div className="c-pb-full">
+          <div class="c-pb-full">
             <Button variant="contained" href="#games">
               Join a livestream
             </Button>
@@ -220,10 +184,8 @@ export default function LandingPage() {
           <h2>Puzzle games</h2>
         </div>
         <div className="c-fx-column footer-item">
-          <span>
-            Puzzle games is an online website which offers free to play games
-          </span>
-          <span style={{ fontWeight: "bold" }}>© Puzzle games 2022</span>
+          <span>Puzzle games is an online website which offers free to play games</span>
+          <span style={{ fontWeight: 'bold' }}>© Puzzle games 2022</span>
         </div>
         <div className="c-fx-column footer-item-bold">
           <span>
